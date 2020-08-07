fa7993210a923aa51cdcf8dd592e318f0a96427d
Bibek Saini <itsbibeksaini@gmail.com>
2020-08-07 20:07:31 +0530

    Settings Api :: New project

---
+++ src/main/java/com/tmsolution/settingsapi/SettingsapiApplication.java
@@ -0,0 +1,13 @@
+package com.tmsolution.settingsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettingsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettingsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmsolution/settingsapi/SettingsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmsolution.settingsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
