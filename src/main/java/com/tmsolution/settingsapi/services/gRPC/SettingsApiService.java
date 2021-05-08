15dc6c21786b866ecdabb7303b6dc292f9b652d8
Bibek Saini <itsbibeksaini@gmail.com>
2021-05-08 13:50:47 +0530

    Update

--- src/main/java/com/tmsolution/settingsapi/services/gRPC/SettingsApiService.java
+++ src/main/java/com/tmsolution/settingsapi/services/gRPC/SettingsApiService.java
@@ -1,28 +1,25 @@
 package com.tmsolution.settingsapi.services.gRPC;
 
-
-import com.tmsolution.settingsapi.requestmodels.Toggle2faRequest;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import settingsapi.SettingsApiGrpc;
-import settingsapi.toggle2faRequest;
-import settingsapi.toggle2faResponse;
+import tmsolution.settings.SettingsGrpc;
+import tmsolution.settings.Toggle2faRequest;
+import tmsolution.settings.Toggle2faResponse;
 
 @GrpcService
-public class SettingsApiService extends SettingsApiGrpc.SettingsApiImplBase {
+public class SettingsApiService extends SettingsGrpc.SettingsImplBase {
 
     @Autowired
     private ModelMapper modelMapper;
 
     @Override
-    public void toggle2fa(toggle2faRequest request, StreamObserver<toggle2faResponse> responseObserver) {
+    public void toggle2fa(Toggle2faRequest request, StreamObserver<Toggle2faResponse> responseObserver) {
 //        super.toggle2fa(request, responseObserver);
 
         var toggle2fa = modelMapper.map(request, Toggle2faRequest.class);
         System.out.println("Working gRPC endpoint");
         responseObserver.onCompleted();
-
     }
 }
