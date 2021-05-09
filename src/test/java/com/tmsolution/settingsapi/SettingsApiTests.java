5da2396a6fd41c3db892974ce23affbc9e2330a6
Bibek Saini <itsbibeksaini@gmail.com>
2021-05-09 19:49:31 +0530

    commenting context load test

--- src/test/java/com/tmsolution/settingsapi/SettingsApiTests.java
+++ src/test/java/com/tmsolution/settingsapi/SettingsApiTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SettingsApiTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
