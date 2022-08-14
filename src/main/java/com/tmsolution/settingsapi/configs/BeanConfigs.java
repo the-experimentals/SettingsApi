9295001470720b64ae950c4241fb1138486c3203
Bibek Saini <itsbibeksaini@gmail.com>
2022-08-14 22:00:31 +0530

    chore: OpneAPI adding security requirement

--- src/main/java/com/tmsolution/settingsapi/configs/BeanConfigs.java
+++ src/main/java/com/tmsolution/settingsapi/configs/BeanConfigs.java
@@ -1,8 +1,11 @@
 package com.tmsolution.settingsapi.configs;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,11 +20,24 @@ public class BeanConfigs {
 
     @Bean
     public OpenAPI springShopOpenAPI() {
+
+        final String securityScheme = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info().title("Settings API")
                         .description("Settings API description")
                         .version("v0.0.1-SNAPSHOT")
                 )
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securityScheme))
+                .components(new Components()
+                        .addSecuritySchemes(securityScheme,
+                                new SecurityScheme()
+                                        .name(securityScheme)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ))
                 .externalDocs(new ExternalDocumentation()
                         .description("Settings API")
                         .url("https://github.com/TMExperimentals/SettingsApi/blob/master/README.md"));
