da9f5954ba88c32f70a600ba78c174b567e77feb
Bibek Saini <itsbibeksaini@gmail.com>
2022-08-15 00:40:30 +0530

    chore: enabling context load test

--- src/test/java/com/tmsolution/settingsapi/SettingsApiTests.java
+++ src/test/java/com/tmsolution/settingsapi/SettingsApiTests.java
@@ -1,12 +1,16 @@
 package com.tmsolution.settingsapi;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class SettingsApiTests {
-
-//	@Test
-//	void contextLoads() {
-//	}
-
+    @Test
+    void contextLoads(ApplicationContext context) {
+        assertThat(context).isNotNull();
+    }
 }
