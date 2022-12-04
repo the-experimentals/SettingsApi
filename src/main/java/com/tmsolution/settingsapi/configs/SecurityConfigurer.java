5268f89e774105fb2d79ed90599ae070ac4a13b0
Bibek Saini <itsbibeksaini@gmail.com>
2022-12-04 22:27:49 +0530

    fix: Surpassing Security Auto-Configuration

--- src/main/java/com/tmsolution/settingsapi/configs/SecurityConfigurer.java
+++ src/main/java/com/tmsolution/settingsapi/configs/SecurityConfigurer.java
@@ -3,12 +3,14 @@ package com.tmsolution.settingsapi.configs;
 import com.tmsolution.settingsapi.filters.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfigurer {
 
@@ -21,11 +23,19 @@ public class SecurityConfigurer {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().disable().authorizeHttpRequests()
-                .requestMatchers("/v1/test").permitAll()
-                .requestMatchers("/actuator/**").permitAll()
-                .requestMatchers("/api-docs/**").permitAll()
-                .requestMatchers("/swagger-ui/**").permitAll()
+        http
+                .csrf().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .authorizeHttpRequests()
+                .requestMatchers(
+                        "/v1/test",
+                        "/actuator/**",
+                        "/swagger-ui.html",
+                        "/swagger-ui/",
+                        "/swagger-ui/**",
+                        "/swagger-resources/**",
+                        "/*/api-docs/**").permitAll()
                 .anyRequest().authenticated()
                 .and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
