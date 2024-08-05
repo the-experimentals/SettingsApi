16f362650e30a9b7b448fe3f19484d0fe13a22b0
Bibek Saini <itsbibeksaini@gmail.com>
2024-08-05 00:57:05 -0400

    feat: upgrading dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins{
     jacoco
     java
-    id("io.spring.dependency-management") version "1.1.0"
+    id("io.spring.dependency-management") version "1.1.6"
     id("com.google.protobuf") version "0.9.1" apply false
-    id("org.springframework.boot") version "3.0.5" apply false
+    id("org.springframework.boot") version "3.3.2" apply false
 }
 
 tasks{
@@ -38,7 +38,7 @@ subprojects {
     afterEvaluate {
         dependencyManagement{
             imports {
-                mavenBom("org.springframework.boot:spring-boot-dependencies:3.0.5")
+                mavenBom("org.springframework.boot:spring-boot-dependencies:3.3.2")
             }
         }
     }
